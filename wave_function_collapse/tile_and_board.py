1bef4b35fc18b2a7a3b2468acbd25ba6329414a9
Matt Wise <u6813873@anu.edu.au>
2024-02-07 10:46:17 +1100

    init commit

---
+++ wave_function_collapse/tile_and_board.py
@@ -0,0 +1,22 @@
+class TileCreator():
+
+    def __init__(self, tile_directory):
+
+        self.images = []
+
+        # Load all the images from the directory
+        
+
+class Tile():
+
+    def __init__(self, image):
+
+        self.tile = image
+        self.width
+        self.height
+
+        # Scan the edges to get the colour for three sections of each edge
+
+
+
+
