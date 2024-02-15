43d3c280f891c834fd8975e62dcfb3f97c9cb421
Matt Wise <u6813873@anu.edu.au>
2024-02-16 00:28:40 +1100

    running slow

--- flappy_bird/bird_and_pipe.py
+++ flappy_bird/bird_and_pipe.py
@@ -1,7 +1,10 @@
 import pygame
 import random
+import tensorflow as tf
+# import torch
+import numpy as np
 
-class bird():
+class Bird():
 
     def __init__(self, screen):
         self.radius = 10
@@ -9,14 +12,28 @@ class bird():
         self.x = 50
         self.y = 50
         self.yvelocity = 0
-        self.colour = (255, 255, 255)
+        self.colour = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         self.screen = screen
+        self.brain = Brain()
+        self.score = 0
+
+    def __hash__(self):
+        return hash(self.colour)
+    
+    def __eq__(self, other):
+        if isinstance(other, Bird):
+            return self.colour == other.colour
 
     def draw(self):
-        pygame.draw.circle(self.screen, self.colour, (self.x, self.y))
+        pygame.draw.circle(self.screen, self.colour, (self.x, self.y), self.radius)
     
+    def choice(self, pipe_dst, gap_top, gap_bot):
+        if self.brain.predict(self.y, self.yvelocity, pipe_dst, gap_top, gap_bot) >= 0.5:
+            self.jump()
+
     def jump(self):
-        self.yvelocity += 10
+        if not self.yvelocity < 0:
+            self.yvelocity -= 15
 
     def update(self):
         # update y position
@@ -24,45 +41,73 @@ class bird():
 
         # update velocity
         if self.yvelocity < 10:
-            self.yvelocity -= self.gravity
+            self.yvelocity += self.gravity
 
-    def check_collision(self, pipe):
-        # Check if bird in within pipe's area
-
+    def collides(self, pipe):
+        # Check if bird is within pipe's area
         # Check x position is overlapping
-        if self.x > pipe.toplft[0] and self.x < pipe.toplft[0] + self.width:
-
+        if self.x > pipe.toplft[0] and self.x < pipe.toplft[0] + pipe.width:
             # Check y position is above or below pipe gap
-            if self.y < pipe.gapy and self.y > pipe.gapy + pipe.gapdst:
-
+            if self.y < pipe.gapy or self.y > pipe.gapy + pipe.gapdst:
                 return True
-            
         return False
+    
+    def out_of_bounds(self):
+        if self.y < 0 or self.y > self.screen.get_height():
+            return True
+        return False
+    
+    def set_score(self, score):
+        self.score = score
 
-class pipe():
+
+class Pipe():
 
     def __init__(self, screen):
         self.screen = screen
-        self.width = 30
-        self.height = self.screen.height
-        self.toplft = (self.screen.width - self.width, 0)
+        self.width = 60
+        self.height = self.screen.get_height()
+        self.toplft = (self.screen.get_width(), 0)
         self.xvelocity = -10
         self.colour = (255, 255, 255)
 
         # gapy is the the y coordinate for start of gap
-        self.gapy = random.randint(0, self.screen.height)
+        self.gapy = random.randint(self.height * 0.1, self.height * 0.6)
         # gapdst is the height of the gap
-        self.gapdst = 50
+        self.gapdst = 100
 
     def draw(self):
         pygame.draw.rect(self.screen, self.colour, 
                          (self.toplft[0], 0, 
-                          self.width, self.height - self.gapy))
+                          self.width, self.gapy))
         pygame.draw.rect(self.screen, self.colour,
                          (self.toplft[0], self.gapdst + self.gapy,
-                          self.width, self.height - self.gapy + self.gapdst))
+                          self.width, self.height - self.gapy - self.gapdst))
 
     def update(self):
         # update toplft coordinates
         self.toplft = (self.toplft[0] + self.xvelocity, 0)
         
+
+class Brain():
+
+    def __init__(self):
+        self.model = tf.keras.Sequential([
+            tf.keras.layers.Input(shape=(5,)),              # Input layer with 6 inputs
+            tf.keras.layers.Dense(2, activation='relu'),    # Hidden layer with 4
+            tf.keras.layers.Dense(1, activation='sigmoid')  # Output layer with 1 
+        ])
+
+        # self.model = torch.
+
+    
+    def predict(self, y_pos, y_vel, pipe_dst, gap_top, gap_bot):
+
+        input_data = [y_pos, y_vel, pipe_dst, gap_top, gap_bot]
+
+        input_data = np.array(input_data)
+
+        input_data = input_data.reshape(-1,5)
+
+        prediction = self.model.predict(input_data)
+        return prediction
--- flappy_bird/play.py
+++ flappy_bird/play.py
@@ -1,13 +1,15 @@
 import pygame
 import sys
-import bird_and_pipe
+from bird_and_pipe import Bird, Pipe
+import random
+import copy
 
-def main():
+def run():
 
     pygame.init()
 
-    width = 400
-    height = 400
+    width = 800
+    height = 600
 
     black = (0,0,0)
 
@@ -15,23 +17,107 @@ def main():
     clock = pygame.time.Clock()
 
     screen = pygame.display.set_mode((width, height))
-    pygame.display.set("flappy bird with neural net")
+    pygame.display.set_caption("flappy bird with neural net")
 
-    # game objects
-    player = None
+    # Set pipe generation interval
+    INTERVAL = 800
 
+    # Start the timer
+    timer = pygame.time.get_ticks()
+
+    generation = 0
+
+    # Outer while loop initialises each new generation
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
 
-        screen.fill(black)
+        # Initialise game objects
+        birds = {Bird(screen) for _ in range(2)}
+        # birds = {Bird(screen)}
+        dead_birds = set()
+        pipes = [Pipe(screen)]
+
+        # Store reference to the pipe closest to
+        # and moving towards player
+        current_pipe = pipes[0]
+        bird_x_pos = copy.copy(birds).pop().x
+        # max_pipe_dst = current_pipe.toplft[0] - bird_x_pos
+        # print(max_pipe_dst)
+        # TODO: Find a better way to calculate max distance to pipe
+        max_pipe_dst = 120
+
+        generation += 1
+
+        # Count the number of pipes which have passed the player
+        passed_pipes = 0
+
+        # Inner while loop runs a given generation
+        while birds:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            screen.fill(black)
+
+            clock.tick(FPS)
+
+            current_time = pygame.time.get_ticks()
+
+            time_passed = current_time - timer        
+
+            # Add a new pipe every second second
+            if time_passed >= INTERVAL:
+                timer = current_time
+                pipes.append(Pipe(screen))
+
+            # Update current pipe
+            if current_pipe.toplft[0] <= bird_x_pos:
+                current_pipe = pipes[1]
+                # max_pipe_dst = current_pipe.toplft[0] - bird_x_pos
+                passed_pipes += 1
+                # print(max_pipe_dst)
+
+                # Check if 0th pipe is offscreen
+                if pipes[0].toplft[0] + pipes[0].width <= 0:
+                    pipes = pipes[1:]
+
+            # Draw all pipes
+            for pipe in pipes:
+                pipe.draw()
+                pipe.update()
+
+            for bird in copy.copy(birds):
+
+                # Decide whether to jump
+                bird.choice(current_pipe.toplft[0] - bird.x / max_pipe_dst,
+                            current_pipe.gapy / height,
+                            current_pipe.gapy + current_pipe.gapdst / height)
 
 
-        # Add a pair of pipes at the start
-        # And then every third second
+                # height = screen.get_height()
+                # if bird.brain.predict(bird.y / height, 
+                #                         bird.yvelocity / -15, 
+                #                         current_pipe.toplft[0] - bird.x / max_pipe_dst,
+                #                         current_pipe.gapy / height,
+                #                         current_pipe.gapy + current_pipe.gapdst / height) >= 0.5:
+            
+                #     bird.jump()
 
+                bird.update()
+                bird.draw()
+
+                # Check boundary and collision
+                if bird.out_of_bounds() or bird.collides(current_pipe):
+                    dead_birds.add(bird)
+                    birds.remove(bird)
+                    bird.set_score(passed_pipes)
+        
+            pygame.display.flip()
+
+
+def main():
+    
+    run()
 
 if __name__ == "__main__":
     main()
